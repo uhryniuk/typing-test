b6c62f9d7933c4b24bde813db21b4ca3f05dc689
uhryniuk <74523230+uhryniuk@users.noreply.github.com>
2020-12-26 10:18:03 -0600

    Add files via upload
    Roughly Designed page UI, with box positioning to start doing the dirty work.

--- typing-test/type/init.js
+++ typing-test/type/init.js
@@ -1,11 +1,14 @@
 /* Initialize Me! */
+
 console.log("Type Init Script Connected");
 window.onload = function(){
-window.setInterval(function(){
-  // This method of getting user input may be
-  // too laggy to acutally do.
-  // We'd ahve to receive input roughly every 10 miliseconds.
-  // IDK how fun that would work.
-  let currentWord = document.getElementById("word-input").value;
-  console.log(currentWord)}, 15000);
+  let currentWord = document.getElementById("word-input");
+window.setInterval(function(){console.log(currentWord.value)}, 3000);
+  currentWord.addEventListener("keydown", function(e){
+    console.log(e.key);
+    if(e.key == "Space"){
+      console.log("Yaoweee");
+      currentWord.value == null;
+}
+}
 }
